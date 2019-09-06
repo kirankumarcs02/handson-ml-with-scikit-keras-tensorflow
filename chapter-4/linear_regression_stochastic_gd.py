f000f20d2a8a1c47422c153e0242ce1e4a4497f4
kirankumarcs02 <kirankumarcs02@gmail.com>
2019-09-06 19:37:31 +0530

    GD_examples

---
+++ chapter-4/linear_regression_stochastic_gd.py
@@ -0,0 +1,65 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from save_plt_as_image import save_fig
+from sklearn.linear_model import SGDRegressor
+
+
+np.random.seed(42)
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# Where to save the figures
+
+
+X = 4 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+X_b = np.c_[np.ones((100, 1)), X]
+X_new = np.array([[0], [4]])
+X_new_b = np.c_[np.ones((2, 1)), X_new]
+
+# data loaded
+
+theta_path_sgd = []
+m = len(X_b)
+np.random.seed(42)
+
+n_epochs = 50
+t0, t1 = 5, 50  # learning schedule hyperparameters
+
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+theta = np.random.randn(2,1)  # random initialization
+
+for epoch in range(n_epochs):
+    for i in range(m):
+        if epoch == 0 and i < 20:
+            y_predict = X_new_b.dot(theta)
+            style = "b-" if i > 0 else "r--"
+            plt.plot(X_new, y_predict, style)
+        random_index = np.random.randint(m)
+        xi = X_b[random_index:random_index+1]
+        yi = y[random_index:random_index+1]
+        gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(epoch * m + i)
+        theta = theta - eta * gradients
+        theta_path_sgd.append(theta)
+
+plt.plot(X, y, "b.")
+plt.xlabel("$x_1$", fontsize=18)
+plt.ylabel("$y$", rotation=0, fontsize=18)
+plt.axis([0, 2, 0, 15])
+save_fig("sgd_plot")
+plt.show()
+
+######################################################################
+print('------------USING SCIKIT----------------')
+######################################################################
+
+sgd_reg = SGDRegressor(max_iter=50, tol=-np.infty, penalty=None, eta0=0.1, random_state=42)
+sgd_reg.fit(X, y.ravel())
+
+print('theta', theta)
+print("co-ef", sgd_reg.intercept_, sgd_reg.coef_)
