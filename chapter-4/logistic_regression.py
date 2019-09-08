3bbcf0dea8874269b60edba912501d85cf27482d
kirankumarcs02 <kirankumarcs02@gmail.com>
2019-09-08 19:30:56 +0530

    logistic

--- chapter-4/logistic_regression.py
+++ chapter-4/logistic_regression.py
@@ -55,3 +55,78 @@ plt.axis([0, 3, -0.02, 1.02])
 save_fig("logistic_regression_plot")
 plt.show()
 
+###################################################################################################
+
+from sklearn.linear_model import LogisticRegression
+
+X = iris["data"][:, (2, 3)]  # petal length, petal width
+y = (iris["target"] == 2).astype(np.int)
+
+log_reg = LogisticRegression(solver="liblinear", C=10**10, random_state=42)
+log_reg.fit(X, y)
+
+x0, x1 = np.meshgrid(
+        np.linspace(2.9, 7, 500).reshape(-1, 1),
+        np.linspace(0.8, 2.7, 200).reshape(-1, 1),
+    )
+X_new = np.c_[x0.ravel(), x1.ravel()]
+
+y_proba = log_reg.predict_proba(X_new)
+
+plt.figure(figsize=(10, 4))
+plt.plot(X[y==0, 0], X[y==0, 1], "bs")
+plt.plot(X[y==1, 0], X[y==1, 1], "g^")
+
+zz = y_proba[:, 1].reshape(x0.shape)
+contour = plt.contour(x0, x1, zz, cmap=plt.cm.brg)
+
+
+left_right = np.array([2.9, 7])
+boundary = -(log_reg.coef_[0][0] * left_right + log_reg.intercept_[0]) / log_reg.coef_[0][1]
+
+plt.clabel(contour, inline=1, fontsize=12)
+plt.plot(left_right, boundary, "k--", linewidth=3)
+plt.text(3.5, 1.5, "Not Iris-Virginica", fontsize=14, color="b", ha="center")
+plt.text(6.5, 2.3, "Iris-Virginica", fontsize=14, color="g", ha="center")
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.axis([2.9, 7, 0.8, 2.7])
+save_fig("logistic_regression_contour_plot")
+plt.show()
+
+X = iris["data"][:, (2, 3)]  # petal length, petal width
+y = iris["target"]
+
+softmax_reg = LogisticRegression(multi_class="multinomial",solver="lbfgs", C=10, random_state=42)
+softmax_reg.fit(X, y)
+
+x0, x1 = np.meshgrid(
+        np.linspace(0, 8, 500).reshape(-1, 1),
+        np.linspace(0, 3.5, 200).reshape(-1, 1),
+    )
+X_new = np.c_[x0.ravel(), x1.ravel()]
+
+
+y_proba = softmax_reg.predict_proba(X_new)
+y_predict = softmax_reg.predict(X_new)
+
+zz1 = y_proba[:, 1].reshape(x0.shape)
+zz = y_predict.reshape(x0.shape)
+
+plt.figure(figsize=(10, 4))
+plt.plot(X[y==2, 0], X[y==2, 1], "g^", label="Iris-Virginica")
+plt.plot(X[y==1, 0], X[y==1, 1], "bs", label="Iris-Versicolor")
+plt.plot(X[y==0, 0], X[y==0, 1], "yo", label="Iris-Setosa")
+
+from matplotlib.colors import ListedColormap
+custom_cmap = ListedColormap(['#fafab0','#9898ff','#a0faa0'])
+
+plt.contourf(x0, x1, zz, cmap=custom_cmap)
+contour = plt.contour(x0, x1, zz1, cmap=plt.cm.brg)
+plt.clabel(contour, inline=1, fontsize=12)
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.legend(loc="center left", fontsize=14)
+plt.axis([0, 7, 0, 3.5])
+save_fig("softmax_regression_contour_plot")
+plt.show()
