dd44750b116309c0565e2216b3e05be1917ca493
kirankumarcs02 <kirankumarcs02@gmail.com>
2019-09-09 14:34:45 +0530

    preceptrons_example

---
+++ chapter-10/preceptrons.ts
@@ -0,0 +1,86 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from save_plot import save_fig
+
+
+iris = load_iris()
+X = iris.data[:, (2, 3)]
+y = (iris.target == 0).astype(np.int)
+
+per_clf = Perceptron(max_iter=1000, tol=1e-3, random_state=42)
+per_clf.fit(X, y)
+
+y_pred = per_clf.predict([[2, 0.5]])
+
+a = -per_clf.coef_[0][0] / per_clf.coef_[0][1]
+b = -per_clf.intercept_ / per_clf.coef_[0][1]
+
+axes = [0, 5, 0, 2]
+
+x0, x1 = np.meshgrid(
+        np.linspace(axes[0], axes[1], 500).reshape(-1, 1),
+        np.linspace(axes[2], axes[3], 200).reshape(-1, 1),
+    )
+X_new = np.c_[x0.ravel(), x1.ravel()]
+y_predict = per_clf.predict(X_new)
+zz = y_predict.reshape(x0.shape)
+
+plt.figure(figsize=(10, 4))
+plt.plot(X[y==0, 0], X[y==0, 1], "bs", label="Not Iris-Setosa")
+plt.plot(X[y==1, 0], X[y==1, 1], "yo", label="Iris-Setosa")
+
+plt.plot([axes[0], axes[1]], [a * axes[0] + b, a * axes[1] + b], "k-", linewidth=3)
+custom_cmap = ListedColormap(['#9898ff', '#fafab0'])
+
+plt.contourf(x0, x1, zz, cmap=custom_cmap)
+plt.xlabel("Petal length", fontsize=14)
+plt.ylabel("Petal width", fontsize=14)
+plt.legend(loc="lower right", fontsize=14)
+plt.axis(axes)
+
+save_fig("perceptron_iris_plot")
+plt.show()
+
+###########################################################################################################
+print('Activation function')
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+def relu(z):
+    return np.maximum(0, z)
+
+def derivative(f, z, eps=0.000001):
+    return (f(z + eps) - f(z - eps))/(2 * eps)
+
+z = np.linspace(-5, 5, 200)
+
+plt.figure(figsize=(11,4))
+
+plt.subplot(121)
+plt.plot(z, np.sign(z), "r-", linewidth=1, label="Step")
+plt.plot(z, sigmoid(z), "g--", linewidth=2, label="Sigmoid")
+plt.plot(z, np.tanh(z), "b-", linewidth=2, label="Tanh")
+plt.plot(z, relu(z), "m-.", linewidth=2, label="ReLU")
+plt.grid(True)
+plt.legend(loc="center right", fontsize=14)
+plt.title("Activation functions", fontsize=14)
+plt.axis([-5, 5, -1.2, 1.2])
+
+plt.subplot(122)
+plt.plot(z, derivative(np.sign, z), "r-", linewidth=1, label="Step")
+plt.plot(0, 0, "ro", markersize=5)
+plt.plot(0, 0, "rx", markersize=10)
+plt.plot(z, derivative(sigmoid, z), "g--", linewidth=2, label="Sigmoid")
+plt.plot(z, derivative(np.tanh, z), "b-", linewidth=2, label="Tanh")
+plt.plot(z, derivative(relu, z), "m-.", linewidth=2, label="ReLU")
+plt.grid(True)
+#plt.legend(loc="center right", fontsize=14)
+plt.title("Derivatives", fontsize=14)
+plt.axis([-5, 5, -0.2, 1.2])
+
+save_fig("activation_functions_plot")
+plt.show()
