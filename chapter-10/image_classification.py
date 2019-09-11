8a59ae31998de3c6a756ca9680b85cebd4d67341
kirankumarcs02 <kirankumarcs02@gmail.com>
2019-09-11 16:50:58 +0530

    image_classification

---
+++ chapter-10/image_classification.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+from save_plot import save_fig
+import numpy as np
+
+fashion_mnist = keras.datasets.fashion_mnist
+(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+
+X_valid, X_train = X_train_full[:5000] / 255., X_train_full[5000:] / 255.
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+X_test = X_test / 255.
+
+plt.imshow(X_train[0], cmap="binary")
+plt.axis('off')
+plt.show()
+
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+               "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+
+n_rows = 4
+n_cols = 10
+plt.figure(figsize=(n_cols * 1.2, n_rows * 1.2))
+for row in range(n_rows):
+    for col in range(n_cols):
+        index = n_cols * row + col
+        plt.subplot(n_rows, n_cols, index + 1)
+        plt.imshow(X_train[index], cmap="binary", interpolation="nearest")
+        plt.axis('off')
+        plt.title(class_names[y_train[index]], fontsize=12)
+plt.subplots_adjust(wspace=0.2, hspace=0.5)
+save_fig('fashion_mnist_plot', tight_layout=False)
+plt.show()
+
+##############################################################################################
+
+model = keras.models.Sequential()
+model.add(keras.layers.Flatten(input_shape=[28, 28]))
+model.add(keras.layers.Dense(300, activation="relu"))
+model.add(keras.layers.Dense(100, activation="relu"))
+model.add(keras.layers.Dense(10, activation="softmax"))
+
+keras.backend.clear_session()
+np.random.seed(42)
+tf.random.set_seed(42)
+
+print('model.layers', model.layers)
+print('model.summary() = ', model.summary())
+keras.utils.plot_model(model, "my_mnist_model.png", show_shapes=True)
+
+################################################################################################
